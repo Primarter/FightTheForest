e89bdb05145b29419b9e1682bbe4311b82111c01
Blackoutburst <blackoutburst@hotmail.com>
2022-01-13 15:09:54 +0100

    Ability to kill enemies

--- Assets/GetShot.cs
+++ Assets/GetShot.cs
@@ -5,16 +5,31 @@ using UnityEngine;
 public class GetShot : MonoBehaviour
 {
 
+    public int life;
     public LayerMask layer;
+    public GameObject self;
 
-    void Update()
-    {
-        GameObject[] hitpoints = GameObject.FindGameObjectsWithTag("Hit Point");
+    private void Die() {
+        if (life <= 0) {
+            Destroy(self);
+        }
+    }
+
+    private void Hit() {
+        Object[] hitpoints = GameObject.FindGameObjectsWithTag("Hit Point");
         if (hitpoints == null) return;
+
         foreach (GameObject hp in hitpoints) {
             if (Physics.CheckSphere(hp.transform.position, 0.5f, layer)) {
                 Destroy(hp);
+                life -= 10;
             }
         }
+    }
+
+    void Update()
+    {
+        Hit();
+        Die();
     }
 }
